827bb2b16712e1861fac00dd487bf7d74bb92fd0
nipunsm98 <nipunsm98@gmail.com>
2020-05-29 21:47:02 +0530

    Initial Commit

--- COVID-19 (task 01)/src/lk/ac/kln/fct/covid/DataProcessor.java
+++ COVID-19 (task 01)/src/lk/ac/kln/fct/covid/DataProcessor.java
@@ -1,7 +1,7 @@
 package lk.ac.kln.fct.covid;
 
 public class DataProcessor {	
-	private static final String FILE_PATH = "/Users/ASUS/eclipse-workspace/TASK 01/src/covid-19-data.json";
+	private static final String FILE_PATH = "/Users/ASUS/eclipse-workspace/COVID-19 (task 01)/src/covid-19-data.json";
 
 	public static void main(String[] args) {
 		DataReader reader = new DataReader();
